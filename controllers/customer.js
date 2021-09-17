180a7820642f0d2f7a5df29507621218848e68fd
budi-irawan <budi.irawan.171717@gmail.com>
2021-09-17 07:41:40 +0700

    refactoring

--- controllers/customer.js
+++ controllers/customer.js
@@ -1,51 +1,79 @@
 const helperBcrypt = require( '../helper/helperBcrypt' );
 const helperJwt = require( '../helper/helperJwt' );
+const helperValidEmail = require( '../helper/helperValidEmail' );
 const customerModel = require( '../models' ).Customer;
 
 class CustomerController {
-	static register( req, res ) {
+	static async register( req, res ) {
 		const {
 			nama,
 			email,
 			password
 		} = req.body;
 
-		customerModel.findOne( {
-				where: {
-					email: email
-				}
-			} )
-			.then( customer => {
-				if ( customer ) {
-					res.status( 400 ).send( {
-						status: 'ERROR',
-						data: null,
-						error: 'email sudah terdaftar'
-					} )
-				} else {
-					helperBcrypt.buatPassword( password, ( err, passwordHash ) => {
-						customerModel.create( {
-								nama: nama,
-								email: email,
-								password: passwordHash,
-								foto: req.file.filename,
-								status: false,
-								confirmation_code: null,
-								reset_token: null,
-								expire_token: null
-							} )
-							.then( customer => {
-								res.status( 200 ).send( {
-									status: 'OK',
-									data: customer,
-									error: null
-								} )
-							} )
-							.catch( error => res.status( 500 ).send( error ) )
-					} )
-				}
-			} )
-			.catch( error => res.status( 500 ).send( error ) )
+		let errors = {};
+
+		if ( !nama ) {
+			errors.nama = "field nama harus diisi";
+		}
+
+		const emailExist = await customerModel.findOne( {
+			where: {
+				email: email
+			}
+		} );
+
+		if ( !email ) {
+			errors.email = "field email harus diisi";
+		} else if ( helperValidEmail.isEmailValid( email ) == false ) {
+			errors.email = "format email tidak valid";
+		} else if ( emailExist ) {
+			errors.email = "email sudah terdaftar";
+		}
+
+		if ( !password ) {
+			errors.password = "field password harus diisi";
+		} else if ( password.length < 4 ) {
+			errors.password = "password minimal 4 karakter";
+		}
+
+		let filename;
+		if ( !req.file ) {
+			errors.foto = "foto belum diupload";
+		} else {
+			filename = req.file.filename;
+		}
+
+		let response = {};
+		let statusCode;
+
+		if ( Object.entries( errors ).length == 0 ) {
+			const customer = await customerModel.create( {
+				nama: nama,
+				email: email,
+				password: password,
+				foto: filename,
+				status: false,
+				confirmation_code: null,
+				reset_token: null,
+				expire_token: null
+			} );
+			statusCode = 200;
+			response.status = 'success';
+			response.data = {
+				nama: customer.nama,
+				email: customer.email,
+				foto: customer.foto,
+			}
+			response.error = errors;
+		} else {
+			statusCode = 400;
+			response.status = 'client error';
+			response.data = null;
+			response.error = errors;
+		}
+
+		res.status( statusCode ).send( response );
 	}
 
 	static login( req, res ) {
