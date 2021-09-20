06329f1fd93aba8d7317ec626b89b23bc1e6b164
budi-irawan <budi.irawan.171717@gmail.com>
2021-09-20 07:09:09 +0700

    complete registration features

--- controllers/customer.js
+++ controllers/customer.js
@@ -2,6 +2,7 @@ const helperBcrypt = require( '../helper/helperBcrypt' );
 const helperJwt = require( '../helper/helperJwt' );
 const helperValidEmail = require( '../helper/helperValidEmail' );
 const customerModel = require( '../models' ).Customer;
+const bcrypt = require( 'bcrypt' )
 
 class CustomerController {
 	static async register( req, res ) {
@@ -12,23 +13,27 @@ class CustomerController {
 		} = req.body;
 
 		let errors = {};
+		let response = {};
+		let statusCode;
 
 		if ( !nama ) {
 			errors.nama = "field nama harus diisi";
 		}
 
-		const emailExist = await customerModel.findOne( {
-			where: {
-				email: email
-			}
-		} );
-
 		if ( !email ) {
 			errors.email = "field email harus diisi";
 		} else if ( helperValidEmail.isEmailValid( email ) == false ) {
 			errors.email = "format email tidak valid";
-		} else if ( emailExist ) {
-			errors.email = "email sudah terdaftar";
+		} else {
+			const emailExist = await customerModel.findOne( {
+				where: {
+					email: email
+				}
+			} );
+
+			if ( emailExist ) {
+				errors.email = "email sudah terdaftar";
+			}
 		}
 
 		if ( !password ) {
@@ -44,123 +49,342 @@ class CustomerController {
 			filename = req.file.filename;
 		}
 
-		let response = {};
-		let statusCode;
-
-		if ( Object.entries( errors ).length == 0 ) {
-			const customer = await customerModel.create( {
-				nama: nama,
-				email: email,
-				password: password,
-				foto: filename,
-				status: false,
-				confirmation_code: null,
-				reset_token: null,
-				expire_token: null
+		try {
+			let token;
+			let payload = {};
+			payload.email = email;
+			helperJwt.buatToken( payload, ( hasil ) => {
+				token = hasil;
 			} );
-			statusCode = 200;
-			response.status = 'success';
-			response.data = {
-				nama: customer.nama,
-				email: customer.email,
-				foto: customer.foto,
-			}
-			response.error = errors;
-		} else {
-			statusCode = 400;
-			response.status = 'client error';
-			response.data = null;
-			response.error = errors;
-		}
 
-		res.status( statusCode ).send( response );
+			helperBcrypt.buatPassword( password, async ( err, passwordHash ) => {
+				if ( Object.entries( errors ).length == 0 ) {
+					const customer = await customerModel.create( {
+						nama: nama,
+						email: email,
+						password: passwordHash,
+						foto: filename,
+						status: false,
+						confirmation_code: token,
+						reset_token: null,
+						expire_token: null
+					} );
+					statusCode = 200;
+					response.status = 'success';
+					response.data = customer;
+					response.error = errors;
+				} else {
+					statusCode = 400;
+					response.status = 'client error';
+					response.data = null;
+					response.error = errors;
+				}
+				res.status( statusCode ).send( response );
+			} );
+		} catch ( e ) {
+			res.status( 500 ).send( "server error" );
+			console.log( e.message );
+		}
 	}
 
-	static login( req, res ) {
+	static async login( req, res ) {
 		const {
 			email,
 			password
 		} = req.body;
 
-		customerModel.findOne( {
+		let errors = {};
+		let response = {};
+		let statusCode;
+		let token;
+
+		try {
+			const customer = await customerModel.findOne( {
 				where: {
 					email: email
 				}
-			} )
-			.then( customer => {
-				if ( customer ) {
-					if ( customer.status == false ) {
-						res.status( 400 ).send( {
-							status: 'ERROR',
-							data: null,
-							error: 'anda belum verifikasi email'
-						} )
-					} else {
-						helperBcrypt.bandingkanPassword( password, customer.password, ( err, data ) => {
-							if ( data ) {
-								let payload = {};
-								payload.id = customer.id;
-								payload.email = customer.email;
+			} );
 
-								helperJwt.buatToken( payload, ( token ) => {
-									res.status( 200 ).send( {
-										status: 'SUCCESS',
-										data: {
-											token: token
-										},
-										error: null
-									} )
-								} )
-							} else {
-								res.status( 400 ).send( {
-									status: 'ERROR',
-									data: null,
-									error: 'password salah'
-								} )
-							}
-						} )
-					}
+			if ( !email ) {
+				errors.email = "field email harus diisi";
+			} else if ( helperValidEmail.isEmailValid( email ) == false ) {
+				errors.email = "format email tidak valid";
+			} else if ( !customer ) {
+				errors.email = "email belum terdaftar";
+			} else if ( customer.status == false ) {
+				errors.email = "anda belum verifikasi email";
+			} else if ( !password ) {
+				errors.password = "field password harus diisi";
+			} else {
+				const match = await bcrypt.compare( password, customer.password );
+				if ( !match ) {
+					errors.password = "password salah";
 				} else {
-					res.status( 400 ).send( {
-						status: 'ERROR',
-						data: null,
-						error: 'email belum terdaftar'
-					} )
+					let payload = {};
+					payload.id = customer.id;
+					payload.email = customer.email;
+					helperJwt.buatToken( payload, ( hasil ) => {
+						token = hasil;
+					} );
 				}
-			} )
-			.catch( error => res.status( 500 ).send( error ) )
+			}
+
+			if ( Object.entries( errors ).length == 0 ) {
+				statusCode = 200;
+				response.status = 'success';
+				response.data = {
+					token: token
+				};
+				response.error = errors;
+			} else {
+				statusCode = 400;
+				response.status = 'client error';
+				response.data = null;
+				response.error = errors;
+			}
+
+			res.status( statusCode ).send( response );
+		} catch ( e ) {
+			res.status( 500 ).send( "server error" );
+			console.log( e.message );
+		}
 	}
 
-	static getProfile( req, res ) {
-		if ( !req.headers.token ) {
-			res.status( 400 ).send( {
-				status: 'ERROR',
-				data: null,
-				error: 'token tidak ada'
-			} )
-		} else {
-			helperJwt.cekToken( req.headers.token, ( err, decoded ) => {
-				if ( err ) {
-					res.status( 400 ).send( {
-						status: 'ERROR',
-						data: null,
-						error: 'invalid token'
-					} )
+	static async getAllCustomers( req, res ) {
+		try {
+			const data = await customerModel.findAll();
+			let result;
+			if ( data.length > 0 ) {
+				result = data;
+			} else {
+				result = "belum ada data";
+			}
+			const response = {
+				status: 'success',
+				data: result,
+				error: null
+			}
+			res.status( 200 ).send( response );
+		} catch ( e ) {
+			res.status( 500 ).send( "server error" );
+			console.log( e.message );
+		}
+	}
+
+	static async getProfile( req, res ) {
+		const {
+			token
+		} = req.headers;
+
+		let errors = {};
+		let response = {};
+		let statusCode;
+		let email;
+
+		try {
+			if ( !token ) {
+				errors.token = "token tidak ada";
+			} else {
+				helperJwt.cekToken( token, ( err, decoded ) => {
+					if ( err ) {
+						errors.token = "invalid token";
+					} else {
+						email = decoded.email;
+					}
+				} );
+			}
+
+			let customer = {}
+			if ( email ) {
+				customer = await customerModel.findOne( {
+					where: {
+						email: email
+					}
+				} );
+				if ( !customer ) {
+					errors.akun = "akun anda belum terdaftar di aplikasi";
 				} else {
-					customerModel.findOne( {
-							where: {
-								email: decoded.email
-							}
-						} )
-						.then( customer => {
-							res.status( 200 ).send( {
-								status: 'SUCCESS',
-								data: customer,
-								error: null
-							} )
-						} )
-						.catch( error => res.status( 500 ).send( error ) )
+					customer = customer;
 				}
+			}
+
+			if ( Object.entries( errors ).length == 0 ) {
+				statusCode = 200;
+				response.status = 'success';
+				response.data = customer;
+				response.error = errors;
+			} else {
+				statusCode = 400;
+				response.status = 'client error';
+				response.data = null;
+				response.error = errors;
+			}
+
+			res.status( statusCode ).send( response );
+		} catch ( e ) {
+			res.status( 500 ).send( "server error" );
+			console.log( e.message );
+		}
+	}
+
+	static async verifikasi( req, res ) {
+		const {
+			confirmation_code
+		} = req.params;
+
+		let errors = {};
+		let response = {};
+		let statusCode;
+		let email;
+		try {
+			const customer = await customerModel.findOne( {
+				where: {
+					confirmation_code: confirmation_code
+				}
+			} );
+
+			if ( !customer ) {
+				errors.confirmation_code = "kode verifikasi tidak valid";
+			} else {
+				await customer.update( {
+					status: true
+				} );
+			}
+
+			if ( Object.entries( errors ).length == 0 ) {
+				statusCode = 200;
+				response.status = 'success';
+				response.data = {
+					"status_akun": "sudah melakukan verifikasi"
+				};
+				response.error = errors;
+			} else {
+				statusCode = 400;
+				response.status = 'client error';
+				response.data = null;
+				response.error = errors;
+			}
+
+			res.status( statusCode ).send( response );
+		} catch ( e ) {
+			res.status( 500 ).send( {
+				status: 'server error',
+				data: null,
+				error: e.message
+			} )
+		}
+	}
+
+	static async lupaPassword( req, res ) {
+		const {
+			email
+		} = req.body;
+
+		let errors = {};
+		let response = {};
+		let statusCode;
+
+		try {
+			if ( !email ) {
+				errors.email = "masukkan email anda";
+			} else {
+				const customer = await customerModel.findOne( {
+					where: {
+						email: email
+					}
+				} );
+
+				if ( !customer ) {
+					errors.email = "email belum terdaftar";
+				} else {
+					let tokenReset;
+					let payload = {};
+					payload.email = email;
+					helperJwt.buatToken( payload, ( hasil ) => {
+						tokenReset = hasil;
+					} );
+
+					await customer.update( {
+						reset_token: tokenReset,
+						expire_token: Date.now() + 3600000
+					} );
+				}
+			}
+
+			if ( Object.entries( errors ).length == 0 ) {
+				statusCode = 200;
+				response.status = 'success';
+				response.data = {
+					pesan: "cek email untuk pengaturan ulang password"
+				};
+				response.error = errors;
+			} else {
+				statusCode = 400;
+				response.status = 'client error';
+				response.data = null;
+				response.error = errors;
+			}
+
+			res.status( statusCode ).send( response );
+		} catch ( e ) {
+			res.status( 500 ).send( {
+				status: 'server error',
+				data: null,
+				error: e.message
+			} )
+		}
+	}
+
+	static async resetPassword( req, res ) {
+		const {
+			password
+		} = req.body;
+		const {
+			token
+		} = req.params;
+
+		let errors = {};
+		let response = {};
+		let statusCode;
+
+		try {
+			const customer = await customerModel.findOne( {
+				where: {
+					reset_token: token
+				}
+			} );
+
+			if ( !password ) {
+				errors.password = "field password harus diisi";
+			} else if ( password.length < 4 ) {
+				errors.password = "password minimal 4 karakter";
+			}
+			helperBcrypt.buatPassword( password, async ( err, passwordHash ) => {
+				if ( Object.entries( errors ).length == 0 ) {
+					await customer.update( {
+						password: passwordHash,
+						reset_token: null,
+						expire_token: null
+					} );
+
+					statusCode = 200;
+					response.status = 'success';
+					response.data = "password telah direset";
+					response.error = errors;
+				} else {
+					statusCode = 400;
+					response.status = 'client error';
+					response.data = null;
+					response.error = errors;
+				}
+
+				res.status( statusCode ).send( response );
+			} )
+		} catch ( e ) {
+			res.status( 500 ).send( {
+				status: 'server error',
+				data: null,
+				error: e.message
 			} )
 		}
 	}
--- router/customer/customerRoute.js
+++ router/customer/customerRoute.js
@@ -10,5 +10,9 @@ const customerController = require( '../../controllers/customer' );
 router.post( '/register', upload.single( 'foto' ), customerController.register );
 router.post( '/login', customerController.login );
 router.get( '/profile', customerController.getProfile );
+router.get( '/', customerController.getAllCustomers );
+router.put( '/verifikasi/:confirmation_code', customerController.verifikasi );
+router.post( '/lupa-password', customerController.lupaPassword );
+router.put( '/reset-password/:token', customerController.resetPassword );
 
 module.exports = router;
